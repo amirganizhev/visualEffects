4ff4980947c148b9b7d2703572297550276aaeb5
amir <ganizhev.amir@mail.ru>
2021-01-29 05:00:50 +0300

    добавил форму, при вводе символов в которую, внизу указыавается какая кнопка клавиатуры была нажата, а также ее код и не была ли зажата кнопка shift, файл с кодом keyboardButton.js

---
+++ js/project/keyboardButton.js
@@ -0,0 +1,30 @@
+let keyboardButtonForm = document.querySelector('.keyboardButtonForm');
+let keyboardButtonName = document.querySelector('.keyboardButtonName');
+let keyCode = document.querySelector('.keyCode');
+let shiftPressed = document.querySelector('.shiftPressed');
+
+keyboardButtonForm.onkeypress = (event) => {
+	
+	if (event.keyCode >= 97 & event.keyCode <= 122) {
+		keyboardButtonName.innerHTML = 'Вы ввели маленькую английскую букву';
+	} else if (event.keyCode >= 65 & event.keyCode <= 90) {
+		keyboardButtonName.innerHTML = 'Вы ввели большую английскую букву';
+	} else if (event.keyCode >= 1072 & event.keyCode <= 1103) {
+		keyboardButtonName.innerHTML = 'Вы ввели маленькую русскую букву';
+	} else if (event.keyCode >= 1040 & event.keyCode <= 1071) {
+		keyboardButtonName.innerHTML = 'Вы ввели большую русскую букву';
+	} else if (event.keyCode >= 48 & event.keyCode <= 57) {
+		keyboardButtonName.innerHTML = 'Вы ввели цифру';
+	} else {
+		keyboardButtonName.innerHTML = 'Вы ввели что то другое';
+	}
+
+	keyCode.innerHTML = event.keyCode;
+
+		if (event.shiftKey == true) {
+			shiftPressed.innerHTML = 'Да';
+		} else {
+			shiftPressed.innerHTML = 'Нет';
+		}
+}
+
